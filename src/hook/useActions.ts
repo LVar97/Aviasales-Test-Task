ddebf0a7c032ea665702452cc40024c4ddf3d6f7
Varvara Karelina <letkovaa@mail.ru>
2022-03-21 12:20:55 +0300

    build

--- src/hook/useActions.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as TicketsActionCreators from "../store/action-creators/tickets";
-
-export const useActions = () => {
-	const dispatch = useDispatch();
-	return bindActionCreators(TicketsActionCreators, dispatch)
-};
--- src/store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { ticketsReducer } from "./ticketsReducer";
-
-export const rootReducer = combineReducers({
-	tickets: ticketsReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>
